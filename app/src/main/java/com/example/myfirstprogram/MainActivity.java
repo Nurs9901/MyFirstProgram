aa8024cf450ff1471679124b9a84d75ae9b3223d
user4 <asannursultan99@mail.ru>
2019-12-05 14:07:16 +0300

    add image button

--- app/src/main/java/com/example/myfirstprogram/MainActivity.java
+++ app/src/main/java/com/example/myfirstprogram/MainActivity.java
@@ -2,13 +2,18 @@ package com.example.myfirstprogram;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    int imageNum = 0;
+    Drawable[] drawables;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,21 +23,39 @@ public class MainActivity extends AppCompatActivity {
         final TextView text = findViewById(R.id.text1);
         Button button = findViewById(R.id.knopa);
         final EditText vasia = findViewById(R.id.editText3);
-        Button touch = findViewById(R.id.touch);
+        final Button touch = findViewById(R.id.touch);
+        final ImageView img = findViewById(R.id.image);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 text.setText(vasia.getText());
-
-
             }
         });
+
+        Drawable myIcon = getResources().getDrawable(R.drawable.file1);
+        Drawable youIcon = getResources().getDrawable(R.drawable.file2);
+        Drawable ourIcon = getResources().getDrawable(R.drawable.file3);
+        Drawable[] drawables = new Drawable[3];
+        drawables[0] = myIcon;
+        drawables[1] = youIcon;
+        drawables[2] = ourIcon;
+        this.drawables = drawables;
+
         touch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int
+               img.setImageDrawable(getNextImage());
             }
         }) ;
     }
+    private Drawable getNextImage() {
+       imageNum++;
+       if (imageNum >= drawables.length) {
+           imageNum = 0;
+       }
+        return drawables[imageNum];
+
+    }
+
 }
