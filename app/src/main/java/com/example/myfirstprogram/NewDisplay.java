c42be240c96021d67ca379dafb01fdcead878c75
user4 <asannursultan99@mail.ru>
2019-12-10 14:43:31 +0300

    NewDisplay

---
+++ app/src/main/java/com/example/myfirstprogram/NewDisplay.java
@@ -0,0 +1,14 @@
+package com.example.myfirstprogram;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewDisplay extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_display);
+    }
+}
